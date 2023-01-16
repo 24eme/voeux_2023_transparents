9a9d484f10af396d9231c8999fde1df9da140ca8
NicoDuclos <nicolas@24eme.fr>
2023-01-16 11:42:32 +0100

    Carte superposées dans dossier svg

--- bin/generatepdf.sh
+++ bin/generatepdf.sh
@@ -1,4 +1,5 @@
 cd res
+cp -f ../svg/cartes_superposees.svg .
 inkscape carte_transparent.svg carte_feuille.svg cartes_superposees.svg --export-area-page --batch-process -d 600 --export-type=pdf
 pdftk cartes_superposees.pdf carte_transparent.pdf carte_feuille.pdf cat output cartes.pdf
 cd ..
