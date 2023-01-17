c0b1eec558a2b5d10e33be223625b16e51a65585
Honorine-MC <honorine.mariechristy@gmail.com>
2023-01-17 18:11:19 +0100

    generation à partir d'un fichier csv

--- bin/generatepdf.sh
+++ bin/generatepdf.sh
@@ -1,5 +1,8 @@
 cd res
 cp -f ../svg/cartes_superposees.svg .
 inkscape carte_transparent.svg carte_feuille.svg cartes_superposees.svg --export-area-page --batch-process -d 600 --export-type=pdf
-pdftk cartes_superposees.pdf carte_transparent.pdf carte_feuille.pdf cat output cartes.pdf
+mv cartes_superposees.pdf cartes_superposees_$1.pdf
+mv carte_transparent.pdf carte_transparent_$1.pdf
+mv carte_feuille.pdf carte_feuille_$1.pdf
+pdftk cartes_superposees_$1.pdf carte_transparent_$1.pdf carte_feuille_$1.pdf cat output cartes_$1.pdf
 cd ..
