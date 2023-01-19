f95625f5b3c6e5947ebf0e919a1817b9cbaea108
NicoDuclos <nicolas@24eme.fr>
2023-01-19 10:31:59 +0100

    reformate generate_all

--- bin/generate_all.sh
+++ bin/generate_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! test "$1"; then
-    echo "Donner le fichier csv";
-    exit 1;
+  echo "Donner le fichier csv"
+  exit 1
 fi
 
 dossier="res/"
@@ -17,27 +17,27 @@ suffixe_transparent="carte_transparent.pdf"
 i=1
 sed 1d $1 | while IFS=';' read -r nom couleur_feuille couleur_transparent ligne1 ligne2 ligne3 ligne4
 do
-   line=$(printf "%03d" $i)
-   bash bin/generate.sh "$line" "$nom" "$couleur_feuille" "$couleur_transparent" "$ligne1" "$ligne2" "$ligne3" "$ligne4"
+  line=$(printf "%03d" $i)
+  bash bin/generate.sh "$line" "$nom" "$couleur_feuille" "$couleur_transparent" "$ligne1" "$ligne2" "$ligne3" "$ligne4"
 
-   if (($i%2==0)); then
+  if (($i % 2 == 0)); then
 
-     j=$((i - 1))
+    j=$((i - 1))
 
-     previous=$(printf "%03d" $j)
-     premiere_feuille=$(find "$dossier" | grep "$previous" | grep "$suffixe_feuille")
-     deuxieme_feuille=$(find "$dossier" | grep "$line" | grep "$suffixe_feuille")
-     premier_transparent=$(find "$dossier" | grep "$previous" | grep "$suffixe_transparent")
-     deuxieme_transparent=$(find "$dossier" | grep "$line" | grep "$suffixe_transparent")
+    previous=$(printf "%03d" $j)
+    premiere_feuille=$(find "$dossier" | grep "$previous" | grep "$suffixe_feuille")
+    deuxieme_feuille=$(find "$dossier" | grep "$line" | grep "$suffixe_feuille")
+    premier_transparent=$(find "$dossier" | grep "$previous" | grep "$suffixe_transparent")
+    deuxieme_transparent=$(find "$dossier" | grep "$line" | grep "$suffixe_transparent")
 
-     #verifier que celui d'avant et le sien ont bien été générer sinon ne fait pas la combinaison
-     if [  -n "$premiere_feuille" ] && [  -n "$deuxieme_feuille" ] && [  -n "$premier_transparent" ] && [  -n "deuxieme_transparent" ]; then
-	bash bin/combine_pdf.sh $premiere_feuille $deuxieme_feuille $dossier_print_feuilles/$previous\_$line.pdf
-	pdftk $dossier_print_feuilles/$previous\_$line.pdf $qrcode_pdf cat output $dossier_print_feuilles/$previous\_$line #concatene avec verso
-	mv $dossier_print_feuilles/$previous\_$line $dossier_print_feuilles/$previous\_$line.pdf
-	bash bin/combine_pdf.sh $premier_transparent $deuxieme_transparent $dossier_print_transparents/$previous\_$line.pdf
-     fi
-   fi
+    #verifier que celui d'avant et le sien ont bien été générer sinon ne fait pas la combinaison
+    if [ -n "$premiere_feuille" ] && [ -n "$deuxieme_feuille" ] && [ -n "$premier_transparent" ] && [ -n "deuxieme_transparent" ]; then
+      bash bin/combine_pdf.sh $premiere_feuille $deuxieme_feuille $dossier_print_feuilles/$previous\_$line.pdf
+      pdftk $dossier_print_feuilles/$previous\_$line.pdf $qrcode_pdf cat output $dossier_print_feuilles/$previous\_$line #concatene avec verso
+      mv $dossier_print_feuilles/$previous\_$line $dossier_print_feuilles/$previous\_$line.pdf
+      bash bin/combine_pdf.sh $premier_transparent $deuxieme_transparent $dossier_print_transparents/$previous\_$line.pdf
+    fi
+  fi
 
-   ((i++))
+  ((i++))
 done
