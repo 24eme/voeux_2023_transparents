0224be9f03eacb10fa4b626716f1cd622d6106c0
Vincent LAURENT <vince.laurent@gmail.com>
2023-01-23 15:59:29 +0100

    Script de vérification de pad

---
+++ bin/verifpad.sh
@@ -0,0 +1,8 @@
+curl -s $1 | grep ";" | grep -v "^;" | awk -F ';' '{
+if(length($4) > 32) { print "Ligne n°1 trop longue " length($4) " : " $4 " (Lignes complètes : " $0 ")" }
+else if(length($5) > 32) { print "Ligne n°2 trop longue " length($5) " : " $5 " (Lignes complètes : " $0 ")" }
+else if(length($6) > 32) { print "Ligne n°3 trop longue " length($6) " : " $6 " (Lignes complètes : " $0 ")" }
+else if(length($7) > 32) { print "Ligne n°4 trop longue " length($7) " : " $7 " (Lignes complètes : " $0 ")" }
+else if(length($8) > 0) { print "Trop de lignes (Lignes complètes : " $0 ")" }
+else if(length($9) > 0) { print "Trop de lignes (Lignes complètes : " $0 ")" }
+}'
